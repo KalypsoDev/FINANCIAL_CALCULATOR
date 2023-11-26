e28b4e18965ccf1b796e0691f1c656dbdf38e487
Angela <antunezangela89@gmail.com>
2023-11-26 23:01:40 +0100

    Basic operations and refactor displayOperations

--- public/js/displayOperations.js
+++ public/js/displayOperations.js
@@ -2,3 +2,7 @@ export function displayNumber (userInput) {
     const currentNumber = document.querySelector("#current__operations")
     currentNumber.value = userInput //aquí estoy escribiendo en value
 }
+
+export function initDisplayOperations () {
+    displayNumber("");
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
-import { initInputNumber } from "./InputNumber.js"
-import { initFunctionInProgress } from "./functionInProgress.js"
+import { initInputNumber } from "./InputNumber.js";
+import { initFunctionInProgress } from "./functionInProgress.js";
+import { initBasicOperations } from "./basicOperations.js";
+import { initDisplayOperations } from "./displayOperations.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     initInputNumber();
     initFunctionInProgress();
+    initBasicOperations();
+    initDisplayOperations();
 });
-
